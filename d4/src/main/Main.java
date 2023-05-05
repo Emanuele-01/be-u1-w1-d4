245ba98a0013eac01d48eeb459d3b31abfef4a3c
Emanuele-01 <emanuele.pitoni.2001@gmail.com>
2023-05-05 16:56:59 +0200

    fine parte di esame

--- d4/src/main/Main.java
+++ d4/src/main/Main.java
@@ -4,7 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-
+		playMedia audio = new playMedia("La guerra di Piero", "audio.wave", 6, 3, 3);
+		audio.abbassaVolume();
+		audio.abbassaLuminosità();
+		audio.play();
 	}
 
 }
